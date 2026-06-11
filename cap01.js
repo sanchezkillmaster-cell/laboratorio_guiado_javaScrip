f8345d15756eeeba9646dcf4b9b48db3d517fa43
pacheco333 <santiago2006ortizp@gmail.com>
2026-06-11 12:39:43 -0500

    hola

---
+++ cap01.js
@@ -0,0 +1,8 @@
+console.log("hola mundo")
+
+console.log("mensaje 1");
+console.log("mensaje 2");
+console.log("mensaje 3");
+console.log("mensaje 4");
+
+console.log("los comentarios no aparecen");
---
+++ cap02.js
@@ -0,0 +1,23 @@
+let nombre = "carlos";
+
+console.log(nombre);
+
+let edad = 20 ;
+
+console.log(edad);
+
+edad = 21;
+
+console.log(edad);
+
+const PI = 3.1416;
+
+console.log(PI);
+
+let nombrecompleto = "Juan";
+let edadUsuario = 25;
+let ciudadRecidencial = "bogota";
+
+console.log(nombrecompleto);
+console.log(edadUsuario);
+console.log(ciudadRecidencial);
